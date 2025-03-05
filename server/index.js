7978092d4b8930e7591942cfefc7afe30c09a840
Mana013 <manaza17801524@gmail.com>
2025-03-05 16:03:15 +0700

    create server

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/',(request, response)=>{
+    
+    response.send(`
+        <!doctype html>
+        <html>
+        <head>
+            <title>Express.js</title>
+        </head>
+        <body>
+            <h3>Welcome to Express.js</h3>
+            <b>Express.js  Fast, unopinionated, minimalist<br>
+            wed framework for Node.js</br>
+        </body>
+        </html>
+        
+        `)
+})
+app.listen(port,()=>console.log('Server listening on port' + port))
+
